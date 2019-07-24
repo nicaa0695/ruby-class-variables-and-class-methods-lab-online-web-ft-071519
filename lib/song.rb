7ba3dcf5316efaadb567480e75751735d69d3760
Veronica Lopez <nicaa0695@gmail.com>
2019-07-24 19:20:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,36 @@
 class Song 
   attr_accessor :name, :artist, :genre
-  @@songs = []
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  
   def initialize(name, artist, genre)
     @name=name 
     @artist=artist 
     @genre=genre
+    
+    @@artists << artist 
+    @@genres << genre 
     @@count += 1 
-    @@songs << self
   end
-  @@count = 0 
+ 
   def self.count 
     @@count 
   end
-  def self.genres
-    @@genres = []
+  def self.genres 
+    @@genres.uniq 
   end
-  
+  def self.artists
+    @@artists.uniq 
+  end
+  def self.genre_count
+    @@genres.each_with_object(Hash.new(0)) do |key, hash|
+      hash[key] += 1 
+    end
+  end
+  def self.artist_count
+    @@artists.each_with_object(Hash.new(0)) do |key, hash|
+      hash[key] += 1 
+    end
+  end
 end 
