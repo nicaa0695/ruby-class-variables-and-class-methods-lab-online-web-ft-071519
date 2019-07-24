c3cc08e0c0b27e1e56b746158d34a80fa9c93822
Veronica Lopez <nicaa0695@gmail.com>
2019-07-24 19:08:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,16 @@
 class Song 
   attr_accessor :name, :artist, :genre
+  @@songs = []
   def initialize(name, artist, genre)
     @name=name 
     @artist=artist 
     @genre=genre
     @@count += 1 
+    @@songs << self
   end
   @@count = 0 
   def self.count 
     @@count 
   end
-  def artists=(artist)
-    @@artists = artist 
-  end
+  
 end 
